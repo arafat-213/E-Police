eeb248ba6d4cfea560b4920526668fec1a802803
arafat-213 <tai.arafat.at@gmail.com>
2019-01-25 16:15:18 +0530

    integrated database with complaint and feedback activity

---
+++ app/src/main/java/com/svit/epolice/Models/Complaint.java
@@ -0,0 +1,71 @@
+package com.svit.epolice.Models;
+
+import java.util.Arrays;
+
+public class Complaint {
+    private String[] media;
+    private String area, address, description;
+    private boolean isAnonymous;
+
+    public Complaint() {
+    }
+
+    public Complaint(String[] media, String area, String address, String description, boolean isAnonymous) {
+        this.media = media;
+        this.area = area;
+        this.address = address;
+        this.description = description;
+        this.isAnonymous = isAnonymous;
+    }
+
+    public String[] getMedia() {
+        return media;
+    }
+
+    public void setMedia(String[] media) {
+        this.media = media;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAnonymous() {
+        return isAnonymous;
+    }
+
+    public void setAnonymous(boolean anonymous) {
+        isAnonymous = anonymous;
+    }
+
+    @Override
+    public String toString() {
+        return "Complaint{" +
+                "media=" + Arrays.toString(media) +
+                ", area='" + area + '\'' +
+                ", address='" + address + '\'' +
+                ", description='" + description + '\'' +
+                ", isAnonymous=" + isAnonymous +
+                '}';
+    }
+}
--- app/src/main/java/com/svit/epolice/activities/FeedbackActivity.java
+++ app/src/main/java/com/svit/epolice/activities/FeedbackActivity.java
@@ -1,63 +1,50 @@
 package com.svit.epolice.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.svit.epolice.Models.Feedback;
 import com.svit.epolice.R;
 
 import java.util.ArrayList;
 
-public class FeedbackActivity extends AppCompatActivity {
+public class FeedbackActivity extends AppCompatActivity implements View.OnClickListener {
 
     ArrayList<String> policeStationsArrayList;
     ArrayList<String> policemenArrayList;
     Spinner policeStationsSpinner, policeMenSpinner;
+    ArrayAdapter policeStationsAdapter;
+    ArrayAdapter policeMenArrayAdapter;
+    ArrayList<String> ManjalpurPoliceMen;
+    ArrayList<String> oldCityPoliceMen;
+    CheckBox anonymousCB;
+    FirebaseDatabase mDatabase;
+    DatabaseReference mFeedbackRef;
+    RatingBar policeRatingBar;
+    EditText descriptionET;
+    Button submitBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feedback);
+        setContentView(R.layout.feedback);
 
-        policeStationsSpinner = findViewById(R.id.policeStationsSpinner);
-        policeMenSpinner = findViewById(R.id.policeMenSpinner);
-        policeStationsArrayList = new ArrayList<String>();
-        policemenArrayList = new ArrayList<String>();
+        init();
 
-        policeStationsArrayList.add("Old City");
-        policeStationsArrayList.add("Karelibag");
-        policeStationsArrayList.add("Manjalpur");
-        policeStationsArrayList.add("Gotri");
-        policeStationsArrayList.add("Sayajigunj");
-
-        policemenArrayList.add("Bajirao Singham");
-        policemenArrayList.add("Jaykant sikhre");
-        policemenArrayList.add("Constable Patil");
-        policemenArrayList.add("Hawaldar Gokhle");
+        mDatabase = FirebaseDatabase.getInstance();
+        mFeedbackRef = mDatabase.getReference("feedback");
 
 
-        final ArrayList<String> ManjalpurPoliceMen = new ArrayList<>();
-        ManjalpurPoliceMen.add("Policewala kaka");
-
-        final ArrayList<String> oldCityPoliceMen = new ArrayList<>();
-        oldCityPoliceMen.add("S.P.Shinchan");
-        oldCityPoliceMen.add("Officer Jenny");
-
-        ArrayAdapter policeStationsAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, policeStationsArrayList);
-        policeStationsSpinner.setAdapter(policeStationsAdapter);
-
-        final ArrayAdapter policeMenArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, policemenArrayList);
-        policeMenSpinner.setAdapter(policeMenArrayAdapter);
-
-        /*policeStationsSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-        });
-        */
         policeStationsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -86,5 +73,63 @@ public class FeedbackActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void init() {
+        policeStationsSpinner = findViewById(R.id.policeStationsSpinner);
+        policeMenSpinner = findViewById(R.id.policeMenSpinner);
+        anonymousCB = findViewById(R.id.anonymousCB);
+        policeRatingBar = findViewById(R.id.policeRatingBar);
+        descriptionET = findViewById(R.id.descriptionET);
+        submitBTN = findViewById(R.id.submitBTN);
+        submitBTN.setOnClickListener(this);
+        policeStationsArrayList = new ArrayList<String>();
+        policemenArrayList = new ArrayList<String>();
+        policeStationsArrayList.add("Old City");
+        policeStationsArrayList.add("Karelibag");
+        policeStationsArrayList.add("Manjalpur");
+        policeStationsArrayList.add("Gotri");
+        policeStationsArrayList.add("Sayajigunj");
+
+        policemenArrayList.add("Bajirao Singham");
+        policemenArrayList.add("Jaykant sikhre");
+        policemenArrayList.add("Constable Patil");
+        policemenArrayList.add("Hawaldar Gokhle");
+
+
+        ManjalpurPoliceMen = new ArrayList<>();
+        ManjalpurPoliceMen.add("Policewala kaka");
+
+        oldCityPoliceMen = new ArrayList<>();
+        oldCityPoliceMen.add("S.P.Shinchan");
+        oldCityPoliceMen.add("Officer Jenny");
+
+        policeStationsAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, policeStationsArrayList);
+        policeStationsSpinner.setAdapter(policeStationsAdapter);
+
+        policeMenArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, policemenArrayList);
+        policeMenSpinner.setAdapter(policeMenArrayAdapter);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.submitBTN:
+                String username = "anonymous";
+                Feedback feedback;
+                if (!anonymousCB.isChecked()) {
+                    username = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+                }
+                feedback = new Feedback(
+                        policeStationsSpinner.getSelectedItem().toString(),
+                        policeMenSpinner.getSelectedItem().toString(),
+                        policeRatingBar.getRating(),
+                        descriptionET.getText().toString(),
+                        username
+                );
+                String key = mFeedbackRef.push().getKey();
+                mFeedbackRef.child(key).setValue(feedback);
+        }
     }
 }
