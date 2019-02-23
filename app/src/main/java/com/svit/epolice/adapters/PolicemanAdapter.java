3450ed319e44c800d26e58ba35a28fabf01044dd
pranal08 <38245830+pranal08@users.noreply.github.com>
2019-02-23 22:11:01 +0530

    fetching policemen images from storage for PolicemenListActivity

--- app/src/main/java/com/svit/epolice/adapters/PolicemanAdapter.java
+++ app/src/main/java/com/svit/epolice/adapters/PolicemanAdapter.java
@@ -1,5 +1,6 @@
 package com.svit.epolice.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.svit.epolice.Models.Policeman;
@@ -16,15 +18,18 @@ import com.svit.epolice.R;
 
 public class PolicemanAdapter extends FirebaseRecyclerAdapter<Policeman, PolicemanAdapter.PolicemanViewHolder> {
 
-
+    private Context context;
     /**
      * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
      * {@link FirebaseRecyclerOptions} for configuration options.
      *
      * @param options
      */
-    public PolicemanAdapter(@NonNull FirebaseRecyclerOptions<Policeman> options) {
+
+
+    public PolicemanAdapter(@NonNull FirebaseRecyclerOptions<Policeman> options, Context context) {
         super(options);
+        this.context = context;
     }
 
     @NonNull
@@ -37,13 +42,14 @@ public class PolicemanAdapter extends FirebaseRecyclerAdapter<Policeman, Policem
 
     @Override
     protected void onBindViewHolder(@NonNull PolicemanViewHolder policemanViewHolder, int position, @NonNull Policeman model) {
-        policemanViewHolder.policemanImage.setImageResource(model.getImage_id());
+        //policemanViewHolder.policemanImage.setImageResource(model.getImage_id());
+        Glide.with(context).load(model.getImage_id()).circleCrop().into(policemanViewHolder.policemanImage);
         policemanViewHolder.policemanName.setText(model.getName());
         policemanViewHolder.policemanRank.setText(model.getRank());
         policemanViewHolder.policemanArea.setText(model.getArea());
         policemanViewHolder.policemanPhone.setText(model.getMobile_no());
         policemanViewHolder.policemanEmail.setText(model.getEmail());
-        policemanViewHolder.policemanRatingTV.setText(model.getRating() + " ★");
+       // policemanViewHolder.policemanRatingTV.setText(model.getRating() + " ★");
 //        policemanViewHolder.policemanRating.setRating(model.getRating());
 
     }
