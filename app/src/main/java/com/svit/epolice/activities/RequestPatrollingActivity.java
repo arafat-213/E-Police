2b568893fbe0add3363711ccbe0531964a07ff90
arafat-213 <tai.arafat.at@gmail.com>
2019-03-05 16:52:08 +0530

    loads user profile on request patrolling page now

--- app/src/main/java/com/svit/epolice/activities/RequestPatrollingActivity.java
+++ app/src/main/java/com/svit/epolice/activities/RequestPatrollingActivity.java
@@ -1,6 +1,7 @@
 package com.svit.epolice.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,9 +13,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.svit.epolice.Models.PatrollingRequest;
+import com.svit.epolice.Models.User;
 import com.svit.epolice.R;
 import com.svit.epolice.utilities.DateRangePickerDialog;
 import com.svit.epolice.utilities.SpinnerData;
@@ -41,13 +49,14 @@ public class RequestPatrollingActivity extends AppCompatActivity implements View
     private static final String PATTERN = "dd-MM-yyyy";
     String mFromDate;
     String mToDate;
-
+    FirebaseUser mCurrentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_patrolling);
         init();
         setupSpinner();
+        loadUserProfile();
     }
 
     public void init() {
@@ -64,6 +73,7 @@ public class RequestPatrollingActivity extends AppCompatActivity implements View
         phoneET = findViewById(R.id.requestPhoneET);
         addressET = findViewById(R.id.requestAddressET);
         mProgressBar = findViewById(R.id.requestPB);
+        mProgressBar.setVisibility(View.VISIBLE);
         mRequestsRef = FirebaseDatabase.getInstance().getReference().child("requests");
     }
     @Override
@@ -144,12 +154,40 @@ public class RequestPatrollingActivity extends AppCompatActivity implements View
 
     @Override
     public void sendInput(Calendar startDate, Calendar endDate) {
-
+        // Interface method used to get startDate and endDate from DateRangePickerDialog
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
         mFromDate = simpleDateFormat.format(startDate.getTime());
         fromDateTV.setText(mFromDate);
 
         mToDate = simpleDateFormat.format(endDate.getTime());
         toDateTV.setText(mToDate);
+    }
+
+    public void loadUserProfile() {
+        // Loads user's name, phone number from database and stores it into widgets
+        // mProgressBar.setVisibility(View.VISIBLE);
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (mCurrentUser != null) {
+            Query query = FirebaseDatabase.getInstance().getReference()
+                    .child("users")
+                    .child(mCurrentUser.getUid());
+
+            query.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    User user = dataSnapshot.getValue(User.class);
+                    Log.e(TAG, "onDataChange: User found: " + user);
+                    nameET.setText(user.getName());
+                    phoneET.setText(user.getMobileNumber());
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                }
+            });
+        }
+
     }
 }
